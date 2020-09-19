d20001de183d32c4a6a93b2ce3da163d35e152d7
Bryan White <bryan@leastauthority.com>
2020-09-19 18:34:20 +0200

    triage crash limit

--- cmd/lafuzz/triage.go
+++ cmd/lafuzz/triage.go
@@ -23,19 +23,19 @@ var (
 		},
 	}
 
-	failureLimit     int
+	crashLimit int
 )
 
 func init() {
-	cmdTriage.Flags().IntVar(&failureLimit, "failure-failureLimit", 1000, "maximun number of failing inputs before stopping and showing the summary")
+	cmdTriage.Flags().IntVar(&crashLimit, "crash-limit", 1000, "maximun number of failing inputs before stopping and showing the summary")
 }
 
 func runTriage(cmd *cobra.Command, args []string) error {
 	pkgName := args[0]
 	fuzzFuncName := args[1]
 	testArgsStr := fmt.Sprintf(
-		"test -tags gofuzz -v -run Test%s %s -args -failingLimit %d",
-		fuzzFuncName, pkgName, failureLimit)
+		"test -tags gofuzz -v -run Test%s %s -args -crash-limit %d",
+		fuzzFuncName, pkgName, crashLimit)
 	testArgs := strings.Split(testArgsStr, " ")
 
 	testCmd := exec.Command("go", testArgs...)
--- example/example_fuzz_test.go
+++ example/example_fuzz_test.go
@@ -3,6 +3,8 @@
 package example
 
 import (
+	"flag"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,10 +12,18 @@ import (
 	"github.com/leastauthority/lafuzz/fuzzing"
 )
 
+var crashLimit int
+
+func TestMain(m *testing.M) {
+	flag.IntVar(&crashLimit, "crash-limit", 1000, "number of crashing inputs to test before stopping")
+	flag.Parse()
+	os.Exit(m.Run())
+}
+
 func TestFuzzBuggyFunc(t *testing.T) {
 	_, panics, _ := fuzzing.
 		MustNewCrasherIterator(FuzzBuggyFunc).
-		TestFailingLimit(t, 1000)
+		TestFailingLimit(t, crashLimit)
 
 	require.Zero(t, panics)
 }
