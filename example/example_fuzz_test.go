96414d9e0c95a9199aaeec780cd2b5da0bc02058
Bryan White <bryan@leastauthority.com>
2020-09-28 18:02:49 +0200

    update example triage test

--- example/example_fuzz_test.go
+++ example/example_fuzz_test.go
@@ -4,34 +4,37 @@ package example
 
 import (
 	"flag"
-	"github.com/leastauthority/fleece/fuzzing"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	fleece "github.com/leastauthority/fleece/fuzzing"
 )
 
 var (
-	crashLimit int
-	fleeceDir  string
-	env        *fuzzing.Env
+	limit       int
+	fleeceDir   string
+	skipPattern string
+
+	env *fleece.Env
 )
 
 func init() {
-	flag.IntVar(&crashLimit, "crash-limit", 1000, "number of crashing inputs to test before stopping")
-	flag.StringVar(&fleeceDir, "fleece-dir", "./fleece", "path to the root of fuzz function workdirs")
+	flag.IntVar(&limit, "crash-limit", 1000, "number of crashing inputs to test before stopping")
+	flag.StringVar(&fleeceDir, "fleece-dir", "fleece", "path to fleece dir relative to repo/module root")
+	flag.StringVar(&skipPattern, "skip", "", "if provided, crashers with recorded outputs which match the pattern will be skipped")
 }
 
 func TestMain(m *testing.M) {
 	flag.Parse()
-	env = fuzzing.NewEnv(fleeceDir)
+	env = fleece.NewEnv(fleeceDir)
 
 	os.Exit(m.Run())
 }
 
 func TestFuzzPanickyFunc(t *testing.T) {
-	_, panics, _ := fuzzing.MustNewCrasherIterator(env, FuzzPanickyFunc).
-		TestFailingLimit(t, env, crashLimit)
+	_, panics, _ := fleece.MustNewCrasherIterator(env, FuzzPanickyFunc).
+		TestFailingLimit(t, crashLimit, fleece.SkipFilter(skipPattern))
 
 	require.Zero(t, panics)
 }
